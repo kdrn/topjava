34cf64f712f64822574299a982a59a059ad6e009
kdrn <vitaly.kudrin@gmail.com>
2017-01-19 21:56:11 +0300

    My HW03 with Optional

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -2,11 +2,15 @@ package ru.javawebinar.topjava;
 
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
 /**
  * GKislin
@@ -14,9 +18,10 @@ import java.util.List;
  */
 public class MealTestData {
 
-    public static final Meal TEST_MEAL = new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "тестовое блюдо", 500);
-    public static final Meal TEST_MEAL_WITH_ID = new Meal(100003, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "тестовое блюдо - ОБЕД", 500);
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+    public static final Meal TEST_MEAL = new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "чужое тестовое блюдо", 500);
+    public static final Meal TEST_MEAL_ALIEN = new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "чужое тестовое блюдо", 500);
+    public static final Meal TEST_MEAL_WITH_ID = new Meal(100003, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000);
+//    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
 
     public static final List<Meal> MEALS_LIST = Arrays.asList(
             new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
@@ -27,4 +32,39 @@ public class MealTestData {
             new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
     );
 
+    public static final int MEAL_ID = 100003;
+
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
+            (expected, actual) -> expected == actual ||
+                    (Objects.equals(expected.getId(), actual.getId())
+                            && Objects.equals(expected.getCalories(), actual.getCalories())
+                            && Objects.equals(expected.getDateTime(), actual.getDateTime())
+                            && Objects.equals(expected.getDescription(), actual.getDescription())
+                    )
+    );
+
+    public static final List<Meal> MEALS_LIST_WITHOUT_100003 = Arrays.asList(
+            new Meal(100007, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510),
+            new Meal(100006, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+            new Meal(100005, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(100004, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+            new Meal(100002,LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500)
+    );
+
+    public static final List<Meal> MEALS_LIST_BETWEEN = Arrays.asList(
+            new Meal(100003, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+            new Meal(100002,LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500)
+    );
+
+    public static final List<Meal> MEALS_LIST_ALL = Arrays.asList(
+            new Meal(100007, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510),
+            new Meal(100006, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+            new Meal(100005, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+            new Meal(100004, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+            new Meal(100003, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+            new Meal(100002,LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500)
+    );
+
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
 }
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -2,36 +2,66 @@ package ru.javawebinar.topjava.service;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.DbPopulator;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import static org.junit.Assert.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by kdrn on 19.01.2017.
- */
+import static ru.javawebinar.topjava.MealTestData.*;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
 public class MealServiceTest {
+
+    @Autowired
+    private MealService service;
+
+    @Autowired
+    private DbPopulator populator;
+
     @Before
     public void setUp() throws Exception {
-
+        populator.execute();
     }
 
     @Test
     public void get() throws Exception {
-
+        Meal gotMeal = service.get(MealTestData.MEAL_ID, UserTestData.USER_ID);
+        MATCHER.assertEquals(MealTestData.TEST_MEAL_WITH_ID, gotMeal);
     }
 
     @Test
     public void delete() throws Exception {
-
+        service.delete(MEAL_ID, USER_ID);
+        MATCHER.assertCollectionEquals(MEALS_LIST_WITHOUT_100003, service.getAll(USER_ID));
     }
 
     @Test
     public void getBetweenDateTimes() throws Exception {
-
+        LocalDateTime ldt1 = LocalDateTime.of(2015, Month.MAY, 30, 10, 0);
+        LocalDateTime ldt2 = LocalDateTime.of(2015, Month.MAY, 30, 13, 0);
+        List<Meal> got = new ArrayList<Meal>(service.getBetweenDateTimes(ldt1, ldt2, USER_ID));
+        MATCHER.assertCollectionEquals(MEALS_LIST_BETWEEN, got);
     }
 
     @Test
     public void getAll() throws Exception {
-
+        MATCHER.assertCollectionEquals(MEALS_LIST_ALL, service.getAll(USER_ID));
     }
 
     @Test
@@ -44,4 +74,14 @@ public class MealServiceTest {
 
     }
 
+    @Test(expected = NotFoundException.class)
+    public void testGetNotFound() throws Exception {
+        service.get(1, USER_ID);
+    }
+
+    @Test(expected = NotFoundException.class)
+    public void testAlienGet() throws Exception {
+        Meal newMeal = service.update(TEST_MEAL_ALIEN, ADMIN_ID);
+        service.get(newMeal.getId(), USER_ID);
+    }
 }
